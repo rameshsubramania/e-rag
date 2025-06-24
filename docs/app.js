e26cb7bf6b45443f5a3bf99e644a8a5ebf9ec661
Jeeva G <Jeeva.Govindhan@techxle.com>
2025-06-24 18:39:59 +0530

    Use SharePoint URLs directly from Teams context

--- docs/app.js
+++ docs/app.js
@@ -239,7 +239,7 @@ microsoftTeams.app.initialize().then(() => {
 
         if (powerAppIframe && teamId !== 'Not available' && channelId !== 'Not available') {
             let powerAppUrl = `https://apps.powerapps.com/play/${powerAppId}?source=website`;
-            
+            powerAppUrl += `&sharepointUrl=${encodeURIComponent(generatedSharepointUrl)}`; // THIS LINE
             powerAppUrl += `&FirstName=${encodeURIComponent("Jeeva")}`;
             powerAppUrl += `&tenantId=${encodeURIComponent(tenantId)}`;
             powerAppUrl += `&teamId=${encodeURIComponent(teamId)}`;
@@ -253,7 +253,7 @@ microsoftTeams.app.initialize().then(() => {
             console.log('Hi Jeeva');
             console.log('Debug: Value of generatedSharepointUrl BEFORE append:', generatedSharepointUrl);
 
-    powerAppUrl += `&sharepointUrl=${encodeURIComponent(generatedSharepointUrl)}`; // THIS LINE
+    
 
 
             powerAppIframe.src = powerAppUrl;
