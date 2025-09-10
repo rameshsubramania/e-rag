23ba3581f16be47a6aa26a06261501caf98d50f2
PrasanthGopalakrishnan1411 <prasanth.gopalakrishnan@techxle.com>
2025-09-10 18:45:58 +0530

    prod 98

--- docs/app.js
+++ docs/app.js
@@ -790,7 +790,7 @@ async function saveSettings() {
       console.log('Saving settings:', settings);
       
       // Call the Logic App to save settings to SharePoint
-      const response = await fetch('https://prod-143.westus.logic.azure.com:443/workflows/c10edf5d105a4506b13cd787bb50b1b4/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=s4eBbE9niGQBJq_QK_rmyk-ASgEE3Q-8RF3fVUtXfnk', {
+      const response = await fetch('https://98eeb9e84846efe1a8f46d098c0db3.0e.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/74bc89ba300742f38f1792c3f5b33719/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=y3a9KvtSH_NR5TEqMKPBoQOowmbWCe1PFV6h1D0x6iA', {
           method: 'POST',
           headers: {
               'Content-Type': 'application/json',
